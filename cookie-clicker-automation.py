9d8d2622151e72f20add89857afc559c8de1affb
Leonardo <46436462+araujo88@users.noreply.github.com>
2021-10-24 22:00:04 -0300

    Create cookie-clicker-automation.py

---
+++ cookie-clicker-automation.py
@@ -0,0 +1,33 @@
+# Cookie Clicker automation
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+import os, time
+
+PATH = os.environ.get('CHROMEDRIVER_PATH')
+driver = webdriver.Chrome(PATH)
+driver.get("https://orteil.dashnet.org/cookieclicker")
+
+driver.implicitly_wait(5)
+
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_by_id("cookies")
+
+price0 = 15
+count = 0
+for i in range(5000):
+    actions = ActionChains(driver) # sequence of actions in queue
+    actions.click(cookie)
+    if count > price0:
+        item = driver.find_element(By.ID, "productPrice0")
+        upgrade_actions = ActionChains(driver)
+        upgrade_actions.move_to_element(item)
+        upgrade_actions.click()
+        upgrade_actions.perform()
+        price0 = int(item.text)
+    if i > 1:
+        count = cookie_count.text.split(" ")[0]
+        count.strip()
+        count = int(count)
+        print(count)
+    actions.perform() # perform queued actions
